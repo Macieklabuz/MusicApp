9e9b4d216993147791502497ab2db39cd19ca929
Macieklabuz <xnarhice@gmail.com>
2024-08-12 13:48:45 +0200

    initial commit

---
+++ src/test/java/com/labuz/musicapp/MusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labuz.musicapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
